f8e489e4efbe445ec130a614e478abd0142d8887
lasjorg <28780271+lasjorg@users.noreply.github.com>
2020-06-27 03:04:13 +0200

    add: icons for filter buttons

--- src/App.svelte
+++ src/App.svelte
@@ -111,6 +111,14 @@
     width: 100%;
     margin-top: 20px;
   }
+
+  .filters button {
+    display: flex;
+    align-items: center;
+  }
+  .filters button > svg {
+    margin-right: 5px;
+  }
 </style>
 
 <div class="container">
@@ -127,10 +135,55 @@
 
   <h3>Todo list filters and stats</h3>
   <div class="filters">
-    <button on:click={toggleCompleated}>Completed: {totalCompleated}</button>
+    <button on:click={toggleCompleated}>
+      <svg
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        stroke="currentColor"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
+        <polyline points="22 4 12 14.01 9 11.01" />
+      </svg>
+      {totalCompleated}
+    </button>
+    <button on:click={toggleUncompleated}>
+      <svg
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        stroke="currentColor"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        <circle cx="12" cy="12" r="10" />
+      </svg>
+      {totalUncompleated}
+    </button>
+    <button on:click={toggleAll}>
+      <svg
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        stroke="currentColor"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="css-i6dzq1">
+        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+        <circle cx="12" cy="12" r="3" />
+      </svg>
+      {totalTodos}
+    </button>
+    <!-- <button on:click={toggleCompleated}>Completed: {totalCompleated}</button>
     <button on:click={toggleUncompleated}>
       Uncompleated: {totalUncompleated}
     </button>
-    <button on:click={toggleAll}>All: {totalTodos}</button>
+    <button on:click={toggleAll}>All: {totalTodos}</button> -->
   </div>
 </div>
